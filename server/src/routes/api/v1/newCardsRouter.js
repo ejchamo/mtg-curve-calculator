a1ebb278bd6c64a795abba99aef90b0e8503c616
ejchamo <ejchamo@gmail.com>
2023-11-15 13:41:27 -0500

    update scryfall api call

--- server/src/routes/api/v1/newCardsRouter.js
+++ server/src/routes/api/v1/newCardsRouter.js
@@ -1,18 +1,14 @@
 import express from "express";
-import got from "got";
-import CardSerializer from "../../../serializers/CardSerializer.js";
+import ScryfallClient from "../../../apiClient/ScryfallClient.js";
 
 const newCardsRouter = new express.Router();
 
 newCardsRouter.get("/:name", async (req, res) => {
   const name = req.params.name;
+  const cards = await ScryfallClient.searchByName(name);
 
   try {
-    const response = await got(`https://api.scryfall.com/cards/search?q=name:${name}`);
-    const body = JSON.parse(response.body);
-    const cards = body.data;
-    const serializedCards = CardSerializer.cleanCards(cards);
-    res.status(200).json({ cards: serializedCards });
+    res.status(200).json({ cards });
   } catch (error) {
     console.log(error);
     res.status(500).json({ errors: error });
