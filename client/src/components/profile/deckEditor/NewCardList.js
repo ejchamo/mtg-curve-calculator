d4f725a30ced161cb0068dde467e5686170682d9
ejchamo <ejchamo@gmail.com>
2023-11-16 14:08:23 -0500

    fix error handing of failed card search

--- client/src/components/profile/deckEditor/NewCardList.js
+++ client/src/components/profile/deckEditor/NewCardList.js
@@ -2,12 +2,23 @@ import React from "react";
 import NewCardTile from "./NewCardTile";
 
 const NewCardList = (props) => {
-  const { cards, setCards } = props;
+  const { cards, setCards, searchFailed } = props;
 
   const newCardTiles = props.newCards.map((card) => {
     return <NewCardTile key={card.id} card={card} cards={cards} setCards={setCards} />;
   });
-  return <div className="new-card-list">{newCardTiles}</div>;
+
+  let searchWarning;
+  if (searchFailed) {
+    searchWarning = <div>search failed: try searching by card name</div>;
+  }
+
+  return (
+    <div className="new-card-list">
+      {newCardTiles}
+      {searchWarning}
+    </div>
+  );
 };
 
 export default NewCardList;
--- client/src/components/profile/deckEditor/SearchBox.js
+++ client/src/components/profile/deckEditor/SearchBox.js
@@ -9,14 +9,22 @@ const SearchBox = (props) => {
 
   const [newCards, setNewCards] = useState([]);
   const [showStats, setShowStats] = useState(false);
+  const [searchFailed, setSearchFailed] = useState(false);
 
-  const newCardList = <NewCardList newCards={newCards} cards={cards} setCards={setCards} />;
+  const newCardList = (
+    <NewCardList
+      newCards={newCards}
+      cards={cards}
+      setCards={setCards}
+      searchFailed={searchFailed}
+    />
+  );
   const statsList = <StatsList cards={cards} />;
 
   return (
     <>
       <div className="search-box-top">
-        <SearchBar setNewCards={setNewCards} />
+        <SearchBar setNewCards={setNewCards} setSearchFailed={setSearchFailed} />
         <StatsButton showStats={showStats} setShowStats={setShowStats} />
       </div>
       {showStats ? statsList : newCardList}
