8cb1d6fef0378ad60d299c8ac9bcda7de86ec3c5
ejchamo <ejchamo@gmail.com>
2023-11-17 12:05:02 -0500

    fix import error handling

--- client/src/components/profile/ImportButton.js
+++ client/src/components/profile/ImportButton.js
@@ -3,25 +3,40 @@ import importDeck from "../../services/importDeck";
 
 const ImportButton = (props) => {
   const { decks, setDecks } = props;
+  const [importSuccess, setImportSuccess] = useState(null);
 
   const handleCopy = async () => {
     try {
       const clipboardText = await navigator.clipboard.readText();
       const response = await importDeck(clipboardText);
-      if (response) {
-        const newDeck = response.newDeck;
+      if (response.status === 201) {
+        const body = await response.json();
+        const newDeck = body.newDeck;
         setDecks([...decks, newDeck]);
+        setImportSuccess(true);
+      } else {
+        setImportSuccess(false);
       }
     } catch (error) {
       console.error("Error copying text to clipboard:", error);
     }
   };
 
+  let importStatus;
+
+  if (importSuccess) {
+    importStatus = <div className="import-status"> import successfully loaded</div>;
+  } else if (importSuccess === false) {
+    importStatus = (
+      <div className="import-status"> import failed, the import must be from a MTGA export</div>
+    );
+  }
   return (
     <>
       <button className="button import" onClick={handleCopy}>
         IMPORT DECK FROM MTGA
       </button>
+      {importStatus}
     </>
   );
 };
