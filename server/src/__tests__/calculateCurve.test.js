c32d36d2ef38468e2bac22234f5a91ab004fe04d
ejchamo <ejchamo@gmail.com>
2023-11-21 17:03:18 -0500

    config jest test

--- server/src/__tests__/calculateCurve.test.js
+++ server/src/__tests__/calculateCurve.test.js
@@ -1,7 +1,11 @@
 import calculateCurve from "../scripts/calculateCurve.js";
+import testDeck1 from "../db/seeders/testDeckData/testDeck1.js";
 
 describe("calculateCurve function", () => {
-  it("placeholder", () => {
-    expect("placeholder").toEqual("placeholder");
+  const testResults = calculateCurve(testDeck1.deck, 1000);
+
+  it("returns an object", () => {
+    expect(typeof testResults).toEqual("object");
   });
+
 });
