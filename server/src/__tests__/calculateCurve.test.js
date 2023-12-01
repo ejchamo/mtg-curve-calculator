f960dbb6d546f0cc8ae6e1287f296369bdc3b632
ejchamo <ejchamo@gmail.com>
2023-12-01 17:54:14 -0500

    create basic unit tests

--- server/src/__tests__/calculateCurve.test.js
+++ server/src/__tests__/calculateCurve.test.js
@@ -1,11 +1,71 @@
 import calculateCurve from "../scripts/calculateCurve.js";
 import testDeck1 from "../db/seeders/testDeckData/testDeck1.js";
+import testDeckLands from "../db/seeders/testDeckData/testDeckLands.js";
+import testDeckOneCosts from "../db/seeders/testDeckData/testDeckOneCosts.js";
 
 describe("calculateCurve function", () => {
-  const testResults = calculateCurve(testDeck1.deck, 1000);
+  describe("basic output of function", () => {
+    const testResults = calculateCurve(testDeck1.deck, 1000);
 
-  it("returns an object", () => {
-    expect(typeof testResults).toEqual("object");
+    it("returns an object", () => {
+      expect(typeof testResults).toEqual("object");
+    });
+
+    it("returns an object with 6 keys", () => {
+      const keys = Object.keys(testResults);
+      expect(keys.length).toEqual(6);
+    });
+
+    it("returns an object with 6 keys that are the numbers 1 through 6", () => {
+      const keys = Object.keys(testResults);
+      expect(keys[0]).toEqual("1");
+      expect(keys[1]).toEqual("2");
+      expect(keys[2]).toEqual("3");
+      expect(keys[3]).toEqual("4");
+      expect(keys[4]).toEqual("5");
+      expect(keys[5]).toEqual("6");
+    });
   });
 
+  describe("lands only deck results", () => {
+    const testResults = calculateCurve(testDeckLands.deck, 1000);
+
+    it("returns a 100% chance of drawing land cards", () => {
+      for (const turn in testResults) {
+        expect(testResults[turn].lands).toEqual("100.00");
+      }
+    });
+
+    it("returns no results for cmc (converted mana cost)", () => {
+      for (const turn in testResults) {
+        expect(testResults[turn].cmc1).toEqual(undefined);
+        expect(testResults[turn].cmc2).toEqual(undefined);
+        expect(testResults[turn].cmc3).toEqual(undefined);
+        expect(testResults[turn].cmc4).toEqual(undefined);
+        expect(testResults[turn].cmc5).toEqual(undefined);
+        expect(testResults[turn].cmc6).toEqual(undefined);
+      }
+    });
+  });
+
+  //In a deck with 6 lands and 6 cards that cost 1 land to play, it should always be possible to play a 1 cost card
+  describe("6 lands 6 cmc1 deck results", () => {
+    const testResults = calculateCurve(testDeckOneCosts.deck, 1000);
+
+    it("returns a 100% chance of cmc1 being playable", () => {
+      for (const turn in testResults) {
+        expect(testResults[turn].cmc1).toEqual("100.00");
+      }
+    });
+
+    it("returns no results for cmc2 through cmc6", () => {
+      for (const turn in testResults) {
+        expect(testResults[turn].cmc2).toEqual(undefined);
+        expect(testResults[turn].cmc3).toEqual(undefined);
+        expect(testResults[turn].cmc4).toEqual(undefined);
+        expect(testResults[turn].cmc5).toEqual(undefined);
+        expect(testResults[turn].cmc6).toEqual(undefined);
+      }
+    });
+  });
 });
---
+++ server/src/db/seeders/testDeckData/testDeckOneCosts.js.js
@@ -0,0 +1,246 @@
+const testDeckOneCosts = {
+  deck: [
+    {
+      object: "card",
+      id: "753db072-5d6a-4f37-8f7d-255572ecd3bd",
+      name: "Cut Down",
+      image_uris: {
+        small:
+          "https://cards.scryfall.io/small/front/7/5/753db072-5d6a-4f37-8f7d-255572ecd3bd.jpg?1673307061",
+        normal:
+          "https://cards.scryfall.io/normal/front/7/5/753db072-5d6a-4f37-8f7d-255572ecd3bd.jpg?1673307061",
+        large:
+          "https://cards.scryfall.io/large/front/7/5/753db072-5d6a-4f37-8f7d-255572ecd3bd.jpg?1673307061",
+        png: "https://cards.scryfall.io/png/front/7/5/753db072-5d6a-4f37-8f7d-255572ecd3bd.png?1673307061",
+        art_crop:
+          "https://cards.scryfall.io/art_crop/front/7/5/753db072-5d6a-4f37-8f7d-255572ecd3bd.jpg?1673307061",
+        border_crop:
+          "https://cards.scryfall.io/border_crop/front/7/5/753db072-5d6a-4f37-8f7d-255572ecd3bd.jpg?1673307061",
+      },
+      cmc: 1,
+      type_line: "Instant",
+    },
+    {
+      object: "card",
+      id: "753db072-5d6a-4f37-8f7d-255572ecd3bd",
+      name: "Cut Down",
+      image_uris: {
+        small:
+          "https://cards.scryfall.io/small/front/7/5/753db072-5d6a-4f37-8f7d-255572ecd3bd.jpg?1673307061",
+        normal:
+          "https://cards.scryfall.io/normal/front/7/5/753db072-5d6a-4f37-8f7d-255572ecd3bd.jpg?1673307061",
+        large:
+          "https://cards.scryfall.io/large/front/7/5/753db072-5d6a-4f37-8f7d-255572ecd3bd.jpg?1673307061",
+        png: "https://cards.scryfall.io/png/front/7/5/753db072-5d6a-4f37-8f7d-255572ecd3bd.png?1673307061",
+        art_crop:
+          "https://cards.scryfall.io/art_crop/front/7/5/753db072-5d6a-4f37-8f7d-255572ecd3bd.jpg?1673307061",
+        border_crop:
+          "https://cards.scryfall.io/border_crop/front/7/5/753db072-5d6a-4f37-8f7d-255572ecd3bd.jpg?1673307061",
+      },
+      cmc: 1,
+      type_line: "Instant",
+    },
+    {
+      object: "card",
+      id: "753db072-5d6a-4f37-8f7d-255572ecd3bd",
+      name: "Cut Down",
+      image_uris: {
+        small:
+          "https://cards.scryfall.io/small/front/7/5/753db072-5d6a-4f37-8f7d-255572ecd3bd.jpg?1673307061",
+        normal:
+          "https://cards.scryfall.io/normal/front/7/5/753db072-5d6a-4f37-8f7d-255572ecd3bd.jpg?1673307061",
+        large:
+          "https://cards.scryfall.io/large/front/7/5/753db072-5d6a-4f37-8f7d-255572ecd3bd.jpg?1673307061",
+        png: "https://cards.scryfall.io/png/front/7/5/753db072-5d6a-4f37-8f7d-255572ecd3bd.png?1673307061",
+        art_crop:
+          "https://cards.scryfall.io/art_crop/front/7/5/753db072-5d6a-4f37-8f7d-255572ecd3bd.jpg?1673307061",
+        border_crop:
+          "https://cards.scryfall.io/border_crop/front/7/5/753db072-5d6a-4f37-8f7d-255572ecd3bd.jpg?1673307061",
+      },
+      cmc: 1,
+      type_line: "Instant",
+    },
+    {
+      object: "card",
+      id: "753db072-5d6a-4f37-8f7d-255572ecd3bd",
+      name: "Cut Down",
+      image_uris: {
+        small:
+          "https://cards.scryfall.io/small/front/7/5/753db072-5d6a-4f37-8f7d-255572ecd3bd.jpg?1673307061",
+        normal:
+          "https://cards.scryfall.io/normal/front/7/5/753db072-5d6a-4f37-8f7d-255572ecd3bd.jpg?1673307061",
+        large:
+          "https://cards.scryfall.io/large/front/7/5/753db072-5d6a-4f37-8f7d-255572ecd3bd.jpg?1673307061",
+        png: "https://cards.scryfall.io/png/front/7/5/753db072-5d6a-4f37-8f7d-255572ecd3bd.png?1673307061",
+        art_crop:
+          "https://cards.scryfall.io/art_crop/front/7/5/753db072-5d6a-4f37-8f7d-255572ecd3bd.jpg?1673307061",
+        border_crop:
+          "https://cards.scryfall.io/border_crop/front/7/5/753db072-5d6a-4f37-8f7d-255572ecd3bd.jpg?1673307061",
+      },
+      cmc: 1,
+      type_line: "Instant",
+    },
+    {
+      object: "card",
+      id: "753db072-5d6a-4f37-8f7d-255572ecd3bd",
+      name: "Cut Down",
+      image_uris: {
+        small:
+          "https://cards.scryfall.io/small/front/7/5/753db072-5d6a-4f37-8f7d-255572ecd3bd.jpg?1673307061",
+        normal:
+          "https://cards.scryfall.io/normal/front/7/5/753db072-5d6a-4f37-8f7d-255572ecd3bd.jpg?1673307061",
+        large:
+          "https://cards.scryfall.io/large/front/7/5/753db072-5d6a-4f37-8f7d-255572ecd3bd.jpg?1673307061",
+        png: "https://cards.scryfall.io/png/front/7/5/753db072-5d6a-4f37-8f7d-255572ecd3bd.png?1673307061",
+        art_crop:
+          "https://cards.scryfall.io/art_crop/front/7/5/753db072-5d6a-4f37-8f7d-255572ecd3bd.jpg?1673307061",
+        border_crop:
+          "https://cards.scryfall.io/border_crop/front/7/5/753db072-5d6a-4f37-8f7d-255572ecd3bd.jpg?1673307061",
+      },
+      cmc: 1,
+      type_line: "Instant",
+    },
+    {
+      object: "card",
+      id: "753db072-5d6a-4f37-8f7d-255572ecd3bd",
+      name: "Cut Down",
+      image_uris: {
+        small:
+          "https://cards.scryfall.io/small/front/7/5/753db072-5d6a-4f37-8f7d-255572ecd3bd.jpg?1673307061",
+        normal:
+          "https://cards.scryfall.io/normal/front/7/5/753db072-5d6a-4f37-8f7d-255572ecd3bd.jpg?1673307061",
+        large:
+          "https://cards.scryfall.io/large/front/7/5/753db072-5d6a-4f37-8f7d-255572ecd3bd.jpg?1673307061",
+        png: "https://cards.scryfall.io/png/front/7/5/753db072-5d6a-4f37-8f7d-255572ecd3bd.png?1673307061",
+        art_crop:
+          "https://cards.scryfall.io/art_crop/front/7/5/753db072-5d6a-4f37-8f7d-255572ecd3bd.jpg?1673307061",
+        border_crop:
+          "https://cards.scryfall.io/border_crop/front/7/5/753db072-5d6a-4f37-8f7d-255572ecd3bd.jpg?1673307061",
+      },
+      cmc: 1,
+      type_line: "Instant",
+    },
+    {
+      object: "card",
+      id: "eb604455-c411-414d-a2ef-e7567ee86a4d",
+      name: "Dreamroot Cascade",
+      image_uris: {
+        small:
+          "https://cards.scryfall.io/small/front/e/b/eb604455-c411-414d-a2ef-e7567ee86a4d.jpg?1655879797",
+        normal:
+          "https://cards.scryfall.io/normal/front/e/b/eb604455-c411-414d-a2ef-e7567ee86a4d.jpg?1655879797",
+        large:
+          "https://cards.scryfall.io/large/front/e/b/eb604455-c411-414d-a2ef-e7567ee86a4d.jpg?1655879797",
+        png: "https://cards.scryfall.io/png/front/e/b/eb604455-c411-414d-a2ef-e7567ee86a4d.png?1655879797",
+        art_crop:
+          "https://cards.scryfall.io/art_crop/front/e/b/eb604455-c411-414d-a2ef-e7567ee86a4d.jpg?1655879797",
+        border_crop:
+          "https://cards.scryfall.io/border_crop/front/e/b/eb604455-c411-414d-a2ef-e7567ee86a4d.jpg?1655879797",
+      },
+      cmc: 0,
+      type_line: "Land",
+    },
+    {
+      object: "card",
+      id: "eb604455-c411-414d-a2ef-e7567ee86a4d",
+      name: "Dreamroot Cascade",
+      image_uris: {
+        small:
+          "https://cards.scryfall.io/small/front/e/b/eb604455-c411-414d-a2ef-e7567ee86a4d.jpg?1655879797",
+        normal:
+          "https://cards.scryfall.io/normal/front/e/b/eb604455-c411-414d-a2ef-e7567ee86a4d.jpg?1655879797",
+        large:
+          "https://cards.scryfall.io/large/front/e/b/eb604455-c411-414d-a2ef-e7567ee86a4d.jpg?1655879797",
+        png: "https://cards.scryfall.io/png/front/e/b/eb604455-c411-414d-a2ef-e7567ee86a4d.png?1655879797",
+        art_crop:
+          "https://cards.scryfall.io/art_crop/front/e/b/eb604455-c411-414d-a2ef-e7567ee86a4d.jpg?1655879797",
+        border_crop:
+          "https://cards.scryfall.io/border_crop/front/e/b/eb604455-c411-414d-a2ef-e7567ee86a4d.jpg?1655879797",
+      },
+      cmc: 0,
+      type_line: "Land",
+    },
+    {
+      object: "card",
+      id: "eb604455-c411-414d-a2ef-e7567ee86a4d",
+      name: "Dreamroot Cascade",
+      image_uris: {
+        small:
+          "https://cards.scryfall.io/small/front/e/b/eb604455-c411-414d-a2ef-e7567ee86a4d.jpg?1655879797",
+        normal:
+          "https://cards.scryfall.io/normal/front/e/b/eb604455-c411-414d-a2ef-e7567ee86a4d.jpg?1655879797",
+        large:
+          "https://cards.scryfall.io/large/front/e/b/eb604455-c411-414d-a2ef-e7567ee86a4d.jpg?1655879797",
+        png: "https://cards.scryfall.io/png/front/e/b/eb604455-c411-414d-a2ef-e7567ee86a4d.png?1655879797",
+        art_crop:
+          "https://cards.scryfall.io/art_crop/front/e/b/eb604455-c411-414d-a2ef-e7567ee86a4d.jpg?1655879797",
+        border_crop:
+          "https://cards.scryfall.io/border_crop/front/e/b/eb604455-c411-414d-a2ef-e7567ee86a4d.jpg?1655879797",
+      },
+      cmc: 0,
+      type_line: "Land",
+    },
+    {
+      object: "card",
+      id: "eb604455-c411-414d-a2ef-e7567ee86a4d",
+      name: "Dreamroot Cascade",
+      image_uris: {
+        small:
+          "https://cards.scryfall.io/small/front/e/b/eb604455-c411-414d-a2ef-e7567ee86a4d.jpg?1655879797",
+        normal:
+          "https://cards.scryfall.io/normal/front/e/b/eb604455-c411-414d-a2ef-e7567ee86a4d.jpg?1655879797",
+        large:
+          "https://cards.scryfall.io/large/front/e/b/eb604455-c411-414d-a2ef-e7567ee86a4d.jpg?1655879797",
+        png: "https://cards.scryfall.io/png/front/e/b/eb604455-c411-414d-a2ef-e7567ee86a4d.png?1655879797",
+        art_crop:
+          "https://cards.scryfall.io/art_crop/front/e/b/eb604455-c411-414d-a2ef-e7567ee86a4d.jpg?1655879797",
+        border_crop:
+          "https://cards.scryfall.io/border_crop/front/e/b/eb604455-c411-414d-a2ef-e7567ee86a4d.jpg?1655879797",
+      },
+      cmc: 0,
+      type_line: "Land",
+    },
+    {
+      object: "card",
+      id: "eb604455-c411-414d-a2ef-e7567ee86a4d",
+      name: "Dreamroot Cascade",
+      image_uris: {
+        small:
+          "https://cards.scryfall.io/small/front/e/b/eb604455-c411-414d-a2ef-e7567ee86a4d.jpg?1655879797",
+        normal:
+          "https://cards.scryfall.io/normal/front/e/b/eb604455-c411-414d-a2ef-e7567ee86a4d.jpg?1655879797",
+        large:
+          "https://cards.scryfall.io/large/front/e/b/eb604455-c411-414d-a2ef-e7567ee86a4d.jpg?1655879797",
+        png: "https://cards.scryfall.io/png/front/e/b/eb604455-c411-414d-a2ef-e7567ee86a4d.png?1655879797",
+        art_crop:
+          "https://cards.scryfall.io/art_crop/front/e/b/eb604455-c411-414d-a2ef-e7567ee86a4d.jpg?1655879797",
+        border_crop:
+          "https://cards.scryfall.io/border_crop/front/e/b/eb604455-c411-414d-a2ef-e7567ee86a4d.jpg?1655879797",
+      },
+      cmc: 0,
+      type_line: "Land",
+    },
+    {
+      object: "card",
+      id: "eb604455-c411-414d-a2ef-e7567ee86a4d",
+      name: "Dreamroot Cascade",
+      image_uris: {
+        small:
+          "https://cards.scryfall.io/small/front/e/b/eb604455-c411-414d-a2ef-e7567ee86a4d.jpg?1655879797",
+        normal:
+          "https://cards.scryfall.io/normal/front/e/b/eb604455-c411-414d-a2ef-e7567ee86a4d.jpg?1655879797",
+        large:
+          "https://cards.scryfall.io/large/front/e/b/eb604455-c411-414d-a2ef-e7567ee86a4d.jpg?1655879797",
+        png: "https://cards.scryfall.io/png/front/e/b/eb604455-c411-414d-a2ef-e7567ee86a4d.png?1655879797",
+        art_crop:
+          "https://cards.scryfall.io/art_crop/front/e/b/eb604455-c411-414d-a2ef-e7567ee86a4d.jpg?1655879797",
+        border_crop:
+          "https://cards.scryfall.io/border_crop/front/e/b/eb604455-c411-414d-a2ef-e7567ee86a4d.jpg?1655879797",
+      },
+      cmc: 0,
+      type_line: "Land",
+    },
+  ],
+};
+
+export default testDeckOneCosts;
